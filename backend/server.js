23904ec5b98be502e04a65cd4c3e5c13651222e7
polymarket-backend <nanh46048@gmail.com>
2026-08-25 19:20:52 +0700

    update cors config

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,10 @@ const generateRouter = require('./routes/generate');
 const app = express();
 const PORT = 4000;
 
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({
+  origin: '*', // hoặc 'https://ab150-2.onrender.com'
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/api/quiz', quizRouter);
